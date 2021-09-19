115f18209bccba596a4455e3a66925ed7a1a5796
DaffaAdityaDev <daffaaditya990@gmail.com>
2021-09-19 21:24:06 +0700

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,14 @@
 import Theme from '../styles/theme';
+import React from 'react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <Head>
+        <link rel="shortcut icon" href="images/logo_me.png" />
+        <title>Daffa Aditya Rahman</title>
+    </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
